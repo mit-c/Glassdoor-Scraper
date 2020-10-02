b29b7e313933c0747e4bf249e2952ff0ee1d6cdd
Tim <tim.coulter@bath.edu>
2020-10-02 20:48:28 +0100

    Added feature which shows if reviews are by a current employee or former (assume to be current employee if not specified).

--- glassdoor_scraper.py
+++ glassdoor_scraper.py
@@ -1,4 +1,3 @@
-
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -6,13 +5,15 @@ import matplotlib.dates
 from bs4 import BeautifulSoup
 from datetime import datetime
 
-def glassdoor_scraper(url, num_pages):
 
+def glassdoor_scraper(url, num_pages):
     url_list = [url[:-4] + "_P" + str(i) + ".htm" for i in range(2, num_pages + 1)]
     url_list.append(url)
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"}
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36"}
     times = []
     out_of_5 = []
+    current_employee_list = []
     count = 0
     for url in url_list:
         try:
@@ -20,9 +21,9 @@ def glassdoor_scraper(url, num_pages):
         except:
             print("error 1")
             continue
-        soup = BeautifulSoup(response_content,'html.parser')
+        soup = BeautifulSoup(response_content, 'html.parser')
         try:
-            reviews = soup.find("div",id="ReviewsFeed")
+            reviews = soup.find("div", id="ReviewsFeed")
         except:
             continue
             print("error 2")
@@ -31,26 +32,33 @@ def glassdoor_scraper(url, num_pages):
         except:
             print("error 3")
             continue
+
         count += 1
         percentage = int(count / len(url_list) * 100)
-        if(percentage % 5 == 0):
-            print(percentage, '%' )
+        if (percentage % 1 == 0):
+            print(percentage, '%')
         for review in reviews:
 
             try:
                 the_time_attrs = review.find("time", attrs={"class": "date subtle small"}).attrs
             except:
                 continue
+            try:
+                current_employee = review.find("span", attrs={"class": "authorJobTitle middle"})
+            except:
+                continue
+            current_employee_list.append("Former Employee" not in current_employee.text) # list which says true if current employee. Assumed to be current if not former.
             time_str = the_time_attrs['datetime']
-            datetime_obj = datetime.strptime(time_str[:33],"%a %b %d %Y %H:%M:%S %Z%z")
-            the_review = review.find("div", attrs = {"class": "v2__EIReviewsRatingsStylesV2__ratingNum v2__EIReviewsRatingsStylesV2__small"})
+            datetime_obj = datetime.strptime(time_str[:33], "%a %b %d %Y %H:%M:%S %Z%z")
+            the_review = review.find("div", attrs={
+                "class": "v2__EIReviewsRatingsStylesV2__ratingNum v2__EIReviewsRatingsStylesV2__small"})
             review_txt = float(the_review.text)
             times.append(datetime_obj)
             out_of_5.append(review_txt)
 
     months = matplotlib.dates.MonthLocator()
     years = matplotlib.dates.YearLocator()
-    format = matplotlib.dates.DateFormatter('%d %B %Y') # format for plot
+    format = matplotlib.dates.DateFormatter('%d %B %Y')  # format for plot
 
     new_times = matplotlib.dates.date2num(times)
-    return new_times,out_of_5
+    return new_times, out_of_5, current_employee_list
--- main2.py
+++ main2.py
@@ -2,15 +2,15 @@ import indeed_scraper as indeed
 import glassdoor_scraper as glass
 import matplotlib.pyplot as plt
 import matplotlib.dates
-from datetime import datetime
-from matplotlib.ticker import MaxNLocator
-import scipy.stats as stats
-import seaborn as sns; sns.set()
 import pandas as pd
 import csv
+from matplotlib.ticker import MaxNLocator
+import seaborn as sns
+
+sns.set()
 
 
-def plot_time_scores(times,scores):
+def plot_time_scores(times, scores):
     format = matplotlib.dates.DateFormatter('%d %B %Y')
 
     fig, ax = plt.subplots()
@@ -22,6 +22,7 @@ def plot_time_scores(times,scores):
     plt.show()
     return fig, ax
 
+
 def plot_hist_times(times, scores):
     format = matplotlib.dates.DateFormatter('%d %B %Y')
     fig, ax = plt.subplots()
@@ -30,85 +31,85 @@ def plot_hist_times(times, scores):
     plt.show()
     return fig, ax
 
-def plot_hist_scores(times,scores):
+
+def plot_hist_scores(times, scores):
     fig, ax = plt.subplots()
-    plt.hist(scores, bins=[1,2,3,4,5])
+    plt.hist(scores, bins=[1, 2, 3, 4, 5])
     plt.show()
     return fig, ax
 
 
-def sort_times_scores(times,scores):
-    times_scores = zip(times,scores)
+def sort_times_scores(times, scores):
+    times_scores = zip(times, scores)
     sorted_by_time = sorted(times_scores, key=lambda tuple: tuple[0])
-    times = [time for time,score in sorted_by_time]
-    scores = [score for time,score in sorted_by_time]
-    return times,scores
+    times = [time for time, score in sorted_by_time]
+    scores = [score for time, score in sorted_by_time]
+    return times, scores
 
 
 def plot_moving_average(unsort_times, unsort_scores, step):
-    if(step % 2 == 0):
+    if (step % 2 == 0):
         print("use even step")
-    times,scores = sort_times_scores(unsort_times,unsort_scores)
+    times, scores = sort_times_scores(unsort_times, unsort_scores)
     format = matplotlib.dates.DateFormatter('%d %B %Y')
-    #fig, ax = plt.subplots()
+    # fig, ax = plt.subplots()
     moving_average_score = []
-    moving_time = times[((step)//2)  :-(step)//2 + 1] # for simplicity assuming odd window size.
+    moving_time = times[((step) // 2):-(step) // 2 + 1]  # for simplicity assuming odd window size.
     # if step is 10 then the first datapoint is at 5th point of array so we lose (n-2) points
     # xoo oox
     # 012-321
     num_windows = len(moving_time)
-    i=0
+    i = 0
     while i < len(scores) - step + 1:
-        this_window = scores[i : i+step]
+        this_window = scores[i: i + step]
         window_avg = sum(this_window) / step
         moving_average_score.append(window_avg)
-        i+=1
+        i += 1
 
     df = pd.DataFrame({"Time": moving_time, "Score": moving_average_score})
     print(df.head())
     ax = sns.lineplot(x="Time", y="Score", data=df, estimator=None)
-    #ax.xaxis.set_major_formatter(format)
-    ax.set(ylim=(1,5))
+    # ax.xaxis.set_major_formatter(format)
+    ax.set(ylim=(1, 5))
     plt.gca().yaxis.set_major_locator(MaxNLocator(integer=True))
     plt.gca().xaxis.set_major_formatter(format)
     plt.title(str(len(scores)) + " reviews - moving average")
     plt.show()
     return ax
 
-##### Writing csv
-def write_file(file_name,url ,num_pages):
-    times, scores = glass.glassdoor_scraper(url,num_pages)
-    with open(file_name,'w',newline='') as my_file:
-        writer = csv.writer(my_file)
-        writer.writerow(["times","scores"])
-        writer.writerows(zip(times,scores))
 
-##### End of writing csv
+def write_file(file_name, url, num_pages):
+    times, scores, current_employee= glass.glassdoor_scraper(url, num_pages)
+    with open(file_name, 'w', newline='') as my_file:
+        writer = csv.writer(my_file)
+        writer.writerow(["times", "scores", "current employee"])
+        writer.writerows(zip(times, scores, current_employee))
+
+
 def read_file(file_name):
     times = []
     scores = []
+    current_employee = []
     # maybe make write / read  csv function
     with open(file_name, mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file)
         line_count = 0
         for row in csv_reader:
-            if(line_count == 0):
-                line_count+=1
+            if line_count == 0:
+                line_count += 1
                 continue
             time = row["times"]
             times.append(float(time))
             scores.append(float(row["scores"]))
+            current_employee.append(str(row["current employee"]))
             line_count += 1
-        return times,scores
-
-num_pages = 40
-url = "https://www.glassdoor.co.uk/Reviews/Natwest-Reviews-E36971.htm"
-write_file("NatWest_scores.csv",url ,num_pages)
-times,scores = read_file("NatWest_scores.csv")
+        return times, scores, current_employee
 
 
-plt.scatter(times,scores, s = 5)
+num_pages = 16
+url = "https://www.glassdoor.co.uk/Reviews/Ocado-Technology-Reviews-E995887.htm"
+write_file("NatWest_scores.csv", url, num_pages)
+times, scores, current_employee = read_file("NatWest_scores.csv")
+colour_list = ['g' if boolstr == "True" else 'r' for boolstr in current_employee ]
+plt.scatter(times, scores, s=5, c=colour_list)
 plot_moving_average(times, scores, 51)
-
-
-
